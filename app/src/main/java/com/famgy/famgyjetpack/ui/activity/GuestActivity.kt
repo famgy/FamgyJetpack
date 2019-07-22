34ad443ed8afb540dc8a80ffe1a70cc1d13c8429
famgy <szgpf7285523@gmail.com>
2019-07-22 18:40:53 +0800

    update

--- app/src/main/java/com/famgy/famgyjetpack/ui/activity/GuestActivity.kt
+++ app/src/main/java/com/famgy/famgyjetpack/ui/activity/GuestActivity.kt
@@ -11,12 +11,19 @@ class GuestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_guest)
 
         var window = getWindow()
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-        window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+//        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+//        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+        //全透明
         window.setStatusBarColor(Color.TRANSPARENT);
-        window.setNavigationBarColor(Color.TRANSPARENT)
+        //window.setNavigationBarColor(Color.TRANSPARENT);
+
+        //半透明
+        //window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
     }
 }
---
+++ app/src/main/java/com/famgy/famgyjetpack/util/ScreenInfoUtil.java
@@ -0,0 +1,44 @@
+package com.famgy.famgyjetpack.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.WindowManager;
+
+public class ScreenInfoUtil {
+
+    public static int getStatusBarHeight(Context mContext) {
+        try {
+            Class cls = Class.forName("com.android.internal.R$dimen");
+            String sRes = cls.getField("status_bar_height").get(cls.newInstance()).toString();
+
+            return mContext.getResources().getDimensionPixelSize(Integer.parseInt(sRes));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static int getNavigationBarHeight(Context mContext) {
+        Resources resources = mContext.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height","dimen", "android");
+        int height = resources.getDimensionPixelSize(resourceId);
+
+        return height;
+    }
+
+    /**获取手机屏幕高度*/
+    public static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getRealMetrics(displayMetrics);
+        //wm.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
+    }
+
+    /**将px转换成dp*/
+    public static int dp2px(Context context, float dpVal) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpVal, context.getResources().getDisplayMetrics());
+    }
+}
